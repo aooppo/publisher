909af6e77616cde506184d7ba347e11664c2ee6e
TJ <i@voox.cc>
2020-04-10 17:32:38 +0800

    improve add item

--- src/main/java/com/trinitesolutions/plugin/publisher/PublishType.java
+++ src/main/java/com/trinitesolutions/plugin/publisher/PublishType.java
@@ -1,18 +1,34 @@
 package com.trinitesolutions.plugin.publisher;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public enum PublishType {
     Customer,
-    Item,
+    Item("GenItem"),
     Contacts,
     NULL(false);
 
     private boolean publish = true;
-
+    private List<String> keyList = new ArrayList<>();
     PublishType() {
+        keyList.add(this.name());
+    }
+
+    PublishType(String ... keys) {
+        this();
+        if (keys != null) {
+           Stream.of(keys).forEach(k -> this.keyList.add(k));
+        }
     }
 
     PublishType(boolean publish) {
         this.publish = publish;
+    }
+
+    public List<String> getKeyList() {
+        return keyList;
     }
 
     public boolean canPublish() {
@@ -26,7 +42,7 @@ public enum PublishType {
     public static PublishType forName(String name) {
         if (name == null) return NULL;
         for (PublishType t : values()) {
-            if (t.name().equals(name)) {
+            if ((!t.getKeyList().isEmpty()) && t.getKeyList().contains(name)) {
                 return t;
             }
         }
